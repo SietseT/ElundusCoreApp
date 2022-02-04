b596fe99fcd02d644f53b54ae29c1fcf26eff59b
Sietse Trommelen <3702986+SietseT@users.noreply.github.com>
2022-02-04 15:53:25 +0100

    Fixed seeking and simplified some things

--- src/components/tts/audioplayer.js
+++ src/components/tts/audioplayer.js
@@ -9,8 +9,7 @@ class Audioplayer extends React.Component {
         super();
 
         this.state = {
-            playStatus: true,
-            position: 0,
+            playing: true,
             currentDurationString: "0:00",
             maxDurationStringString: "0:00",
             progressBarWidth: 0,
@@ -27,78 +26,66 @@ class Audioplayer extends React.Component {
     }
 
     componentDidUpdate(prevProps) {
-        // Reset playStatus and position for when a new sound is loaded. 
+        // Reset playing for when a new sound is loaded. 
         if(prevProps.src !== this.props.src) {
+            this.player.seekTo(0);
+
             this.setState({
-                playStatus: true,
-                position: 0
+                playing: true
             });
         }
      }
 
     onProgress(event) {
-
-        console.log(event);
-
         // Calculate width of progressbar in percentage
         let progressBarWidth = event.playedSeconds / event.loadedSeconds * 100;
 
-        // If playStatus is not playing, this may trigger too many state updates
-        if(this.state.playStatus !== true) {
+        // If not playing, this may trigger too many state updates
+        if(this.state.playing !== true) {
             return;
         }
 
         this.loadedSeconds = event.loadedSeconds;
 
+        
+        let maxDurationString = this.getTimeCodeFromNum(this.player.getDuration());
+
         this.setState({
             currentDurationString: this.getTimeCodeFromNum(event.playedSeconds),
-            maxDurationString: this.getTimeCodeFromNum(event.loadedSeconds),
             progressBarWidth: progressBarWidth,
             position: progressBarWidth,
-            playStatus: event.playedSeconds != event.loadedSeconds
+            playing: event.playedSeconds != event.loadedSeconds
         });
+
+        if(this.state.maxDurationString != maxDurationString) {
+            this.setState({
+                maxDurationString: this.getTimeCodeFromNum(this.player.getDuration())
+            });
+        }
     }
 
     togglePlayClick() {
-        let soundState = true;
-        if(this.state.playStatus === true) {
-            this.setState({
-                playStatus: false
-            });
-            return;
-        }
-
-        let position = this.state.position;
-        if(position === this.duration){
-            this.setState({
-                playStatus: soundState,
-                position: 0
-            });
-        }
-        else {
-            this.setState({
-                playStatus: soundState
-            });
-        }
+        this.setState({
+            playing: !this.state.playing
+        });
     }
 
     onMouseClick() {
         // Calculate position to seek to
-        let position = this.duration * (this.progressBarPosition / 100);
+        let position = this.progressBarPosition / 100;
+        this.player.seekTo(position, "fraction");
 
-        //First update position, then trigger state. Simultaneously doesn't work.
-        this.setState({
-            position: position
-        }, () => {
+        if(!this.state.playing) {
             this.setState({
-                playStatus: true
+                playing: true
             });
-        });
+        }
+        
     }
 
     onMouseMove(event) {
         // Needed to calculate position when clicking on seekbar
-        this.progressBarPosition = (event.clientX - this.progressBarRef.current.getBoundingClientRect().left) / event.target.offsetWidth * 100;           
+        this.progressBarPosition = (event.clientX - this.progressBarRef.current.getBoundingClientRect().left) / event.target.offsetWidth * 100;
     }
 
     //turn 128 seconds into 2:08
@@ -110,16 +97,22 @@ class Audioplayer extends React.Component {
         return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
     }
 
+    ref = player => {
+        this.player = player
+      }
+
     render() {
         return (
             <div className="audio-player">                
-            <ReactPlayer url={this.props.src} playing={this.state.playStatus} width='100%' height='100%' onProgress={this.onProgress} />
+            <ReactPlayer url={this.props.src} ref={this.ref} playing={this.state.playing} width='100%' height='100%' 
+            onProgress={this.onProgress} progressInterval="500"
+            />
                 <div className="timeline" ref={this.progressBarRef} onMouseMove={this.onMouseMove} onClick={this.onMouseClick} >
                     <div className="progress" style={{"width": this.state.progressBarWidth + '%'}}></div>
                 </div>
                 <div className="controls">
                     <div className="play-container">
-                        <div className={`toggle-play ${this.state.playStatus === true ? "pause" : "play"}`} onClick={this.togglePlayClick} />
+                        <div className={`toggle-play ${this.state.playing === true ? "pause" : "play"}`} onClick={this.togglePlayClick} />
                     </div>
                     <div className="time">
                         <div className="current">{this.state.currentDurationString}</div>
