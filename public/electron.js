0c8f502013d1338e2bda49dd61a54387b6353209
Manuel Mendivil <mmendivilg@gmail.com>
2022-06-28 07:26:30 -0700

    added copy/paste context menu

--- public/electron.js
+++ public/electron.js
@@ -41,9 +41,59 @@ function createWindow() {
 const isMac = process.platform === 'darwin'
 const template = [
     {
+        label: "Application",
+        submenu: [
+            { 
+                label: "Quit", 
+                accelerator: "Command+Q", 
+                click: function() { 
+                    app.quit(); 
+                }
+            }
+        ]
+    }, 
+    {
         label: 'File',
         submenu: [
             isMac ? { role: 'close' } : { role: 'quit' }
+        ]
+    },
+    {
+        label: "Edit",
+        submenu: [
+            { 
+                label: "Undo", 
+                accelerator: "CmdOrCtrl+Z", 
+                selector: "undo:"
+            },
+            { 
+                label: "Redo", 
+                accelerator: "Shift+CmdOrCtrl+Z", 
+                selector: "redo:" 
+            },
+            { 
+                type: "separator" 
+            },
+            { 
+                label: "Cut", 
+                accelerator: "CmdOrCtrl+X", 
+                selector: "cut:" 
+            },
+            { 
+                label: "Copy", 
+                accelerator: "CmdOrCtrl+C", 
+                selector: "copy:" 
+            },
+            { 
+                label: "Paste", 
+                accelerator: "CmdOrCtrl+V", 
+                selector: "paste:" 
+            },
+            { 
+                label: "Select All", 
+                accelerator: "CmdOrCtrl+A", 
+                selector: "selectAll:" 
+            }
         ]
     },
     {
